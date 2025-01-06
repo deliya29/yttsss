6d1eea0d2975a4b3fbdf96b2df317c1d1c932cc7
deliyasyafa290304 <deliyasyafa@gmail.com>
2025-01-06 16:13:48 +0700

    tiger

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
 import Berita from "./pages/Berita";
 import Contact from "./pages/Contact";
 import BeritaDetail from "./pages/BeritaDetail"; // Import halaman detail berita
 
+// Komponen untuk melacak halaman
+function GoogleAnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag("config", "G-VXGBFVTTFJ", {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      {/* Tambahkan pelacak Google Analytics */}
+      <GoogleAnalyticsTracker />
       <Routes>
         {/* Halaman Beranda */}
         <Route path="/" element={<Home />} />
